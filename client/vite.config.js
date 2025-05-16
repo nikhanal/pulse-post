11b1217821fe1a1f46341acf053aece66b0c5e15
Nishan Khanal <khanal.nishan28@gmail.com>
2025-05-16 20:21:01 +0545

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  }
 })
